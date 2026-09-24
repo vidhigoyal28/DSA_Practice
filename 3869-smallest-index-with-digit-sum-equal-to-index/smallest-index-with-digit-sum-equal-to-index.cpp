354a964d7bf6400b3b80cbeae2ff9ed54df7b2ba
vidhigoyal28 <vidhigoyal280904@gmail.com>
2026-09-24 22:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 31 MB (15.11%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        for(int i = 0; i<nums.size() ;i++){
+            int n = nums[i];
+            int sum =0;
+            while(n >0){
+                sum += n%10;
+                n= n /10;
+            }
+            if(i == sum)
+            return i;
+
+        }
+        return -1;
+    }
+};
